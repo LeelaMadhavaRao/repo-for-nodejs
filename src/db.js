ccf2be715324ca1ff63337ab14c1940985cdfad1
LeelaMadhavaRao <leelamadhav.nulakani@gmail>
2024-08-11 20:29:53 +0530

    changes

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ async function connectToDB(cb) {
     const url = "mongodb+srv://leelamadhavnulakani:9133603383@cluster0.fhipcg5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     const client = new MongoClient(url);
     await client.connect();
-    db = client.db("AST_Database");
+    db = client.db("task1");
     cb();
 }
 
